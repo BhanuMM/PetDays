463ab6118f58697214d26f807d2c7f30a6806abf
BhanuMM <bhanuji_mm@yahoo.com>
2022-07-19 13:00:58 +0530

    User model added'

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require ("express");
 const app = express();
 
-app.listen(3001,()=> {
-    console.log("server running on port 3001");
+
+const db = require("./models");
+
+//Routers will be list down here
+// const usersRouter = require("./routes/Users");
+// app.use("/auth", usersRouter);
+
+
+db.sequelize.sync().then(() => {
+    app.listen(3001,()=> {
+        console.log("server running on port 3001");
+        
+    });
 });
