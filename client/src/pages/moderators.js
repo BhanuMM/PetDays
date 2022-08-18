b4f00b7d6d74248d6d55da8245d7052742cee8f9
kasunikahansamali <kasunikahansamali.96@gmail.com>
2022-08-19 00:10:52 +0530

    dashboard editing

--- client/src/pages/moderators.js
+++ client/src/pages/moderators.js
@@ -7,38 +7,44 @@ function moderators() {
   return (
     <div class="container-fluid">
     <div className="row flex-nowrap">
-       <div className='col-2 bg-dark col-auto col-md-3 col-xl-2 px-sm-2 px-0'>
+       <div className='col-2 side-color col-auto col-md-3 col-xl-2 px-sm-2 px-0'>
           <Sidebar />
        </div>
         <div className="col-10 py-3">
           <Navbar /><hr />
+          <div className='col-8 form-class'>
           <form>
-            <div class="form-group">
+            <div class="mb-3">
               <label for="exampleInputEmail">First Name</label>
               <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
             </div>
-            <div class="form-group">
+            <div class="mb-3">
               <label for="exampleInputEmail">Last Name</label>
               <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
             </div>
-            <div class="form-group">
+            <div class="mb-3">
               <label for="exampleInputPassword">Contact Number</label>
               <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
             </div>
-            <div class="form-group">
+            <div class="mb-3">
               <label for="exampleInputPassword">NIC Number</label>
               <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
             </div>
-            <div class="form-group">
+            <div class="mb-3">
               <label for="exampleInputEmail1">Email address</label>
               <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
             </div>
-            <div class="form-group">
-              <label for="exampleFormControlFile1">Upload your picture here</label>
+            <div class="mb-3">
+              <label for="exampleFormControlFile1">Upload picture here</label>
               <input type="file" class="form-control-file" id="exampleFormControlFile1"/>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <div className='row'>
+                    <div className='col-9'></div>
+                    <div className='col-3 mb-5 mt-5'> <button type="submit" class=" pl-5 btn btn-success">Submit</button></div>
+            </div>
           </form>
+          </div>
+          
       </div>
     </div>
 </div>
