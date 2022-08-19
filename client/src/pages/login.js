a4ac2152e652a84972fb145deb2d5ba8adef8342
vihanga-sen <sandunisenarath98@gmail.com>
2022-08-19 22:28:55 +0530

    added forgot password,verify email uis

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,52 +1,76 @@
 import React from 'react'
-import signin from '../images/petlv.png';
+import signin from '../images/login.png';
+import Bgimage from '../images/bkimage.webp';
 
 import '../styles/login.css';
 
+
 function login() {
   return (
-    <div className="Login">
-        <section className="vh-100 color">
+    <div className="Login login-bgimagesize" style={{ 
+        backgroundImage:`url(${Bgimage})`  
+      }}>
+        <section className="vh-100 ">
             <div className="container py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col col-xl-10">
-                        <div className="card" border-radius="2rem; ">
+                    <div className="col col-xl-10 ">
+                        <div className="card login-cardsize flex-center login-cardmargins" border-radius="2rem; ">
                             <div className="row g-0">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                <div className="col-md-6 col-lg-5 d-none d-md-block ">
                                     <img src={signin}
-                                    alt="login form" className="img-fluid mt-5 pt-5" border-radius="1rem 0 0 1rem;" />
+                                    alt="login form" className="img-fluid mt-5 pt-5 login-imagesize" border-radius="1rem 0 0 1rem;" />
+                                    <div className='mt-3' >
+                                        <h5 className='text-center'>Welcome to </h5>
+                                        <h2 className='text-center login.caption'>PET DAYS</h2>
+                                    </div>
                                 </div>
                                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
                                     <div className="card-body p-4 p-lg-5 text-black">
 
                                         <form>
 
-                                        <div className="d-flex align-items-center mb-3 pb-1">
-                                        <img src= "../images/PetDays.png" className="logo" alt="dog"/>
+                                        
+
+                                        <h3 className="fw-normal mb-3 pb-3 text-center text-bold login.caption" letter-spacing="1px;">LOGIN</h3>
+                                        <p className="pb-lg-2" letter-spacing="#393f81;">Don't have an account? <a href="#!"
+                                            color="#393f81;">SignUP</a></p>
+
+                                        <div class="form-outline mb-4">
+                                            <label className="form-label" for="">E-mail </label>
+                                            <input type="email" id="form2Example17" className=" login-inputfieldwidth form-control form-control-lg" />
+                                            
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <label class="form-label" for="form2Example27">Password</label>
+                                            <input type="password" id="form2Example27" class=" login-inputfieldwidth form-control form-control-lg" />
                                            
                                         </div>
-
-                                        <h5 className="fw-normal mb-3 pb-3" letter-spacing="1px;">Sign into your account</h5>
-
-                                        <div class="form-outline mb-4">
-                                            <input type="email" id="form2Example17" className="form-control form-control-lg" />
-                                            <label className="form-label" for="">Email address</label>
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <input type="password" id="form2Example27" class="form-control form-control-lg" />
-                                            <label class="form-label" for="form2Example27">Password</label>
-                                        </div>
-
+{/* 
                                         <div class="pt-1 mb-4">
-                                            <button className ="btn btn-success btn-lg btn-block" type="button">Login</button>
-                                        </div>
+                                            <button className ="btn btn-success btn-lg btn-block text-center" type="button">Login</button>
+                                        </div> */}
+                                        <div className='row'>
+                                            {/* <div className='col'>
+                                            <input type="checkbox" id="topping" name="topping" value="" />Remembr Me
+                                           
+                                            </div> */}
 
-                                        <a className="small text-muted" href="#!">Forgot password?</a>
-                                        <p className="mb-5 pb-lg-2" letter-spacing="#393f81;">Don't have an account? <a href="#!"
-                                            color="#393f81;">Register here</a></p>
-                                        <a href="#!" className="small text-muted">Terms of use.</a>
-                                        <a href="#!" className="small text-muted">Privacy policy</a>
+                                            
+                                            <div className='col'>
+                                                <a className="small text-muted  ml-5" href="#!">Forgot password?</a>
+
+                                            </div>
+                                        </div>
+                                        <div class="pt-1 mb-4 mt-3 login.buttonmargin text-center">
+                                            <button className ="login.loginbuttonsize login.buttonmargin btn btn-success "  type="button">LOGIN   </button>
+                                        </div>
+                                        
+
+                                       
+                                        
+                                        {/* <a href="#!" className="small text-muted">Terms of use.</a>
+                                        <a href="#!" className="small text-muted">Privacy policy</a> */}
                                         </form>
 
                                     </div>
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,59 +1,70 @@
 import React from 'react'
-import signin from '../images/petlv.png';
+
+import signin from '../images/login.png';
+import Bgimage from '../images/bkimage.webp';
 import '../styles/register.css';
+
 
 function register() {
   return (
-    <div className='Register'>
-         <section className="vh-100 color">
+    <div className='Register' style={{ 
+        backgroundImage:`url(${Bgimage})`  
+      }}>
+         <section className="vh-100 ">
             <div className="container py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col col-xl-10">
-                        <div className="card" border-radius="2rem; ">
+                    <div className="col col-xl-10 ">
+                        <div className="card register-cardsize flex-center register-cardmargins" border-radius="2rem; ">
                             <div className="row g-0">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                <div className="col-md-6 col-lg-5 d-none d-md-block mt-5">
                                     <img src={signin}
-                                    alt="login form" className="img-fluid mt-5 pt-5" border-radius="1rem 0 0 1rem;" />
+                                    alt="login form" className="img-fluid mt-5 pt-5 imagesize" border-radius="1rem 0 0 1rem;" />
+                                    <div >
+                                        <h5 className='text-center'>Welcome to </h5>
+                                        <h2 className='text-center register.caption'>PET DAYS</h2>
+                                    </div>
                                 </div>
                                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
                                     <div className="card-body p-4 p-lg-5 text-black">
 
                                         <form>
 
-                                        <div className="d-flex align-items-center mb-3 pb-1">
-                                        <img src= "../images/PetDays.png" className="logo" alt="dog"/>
+                                        
+
+                                        <h3 className="fw-normal mb-3 pb-3 text-center text-bold register.caption" letter-spacing="1px;">CREATE YOUR ACCOUNT</h3>
+                                        <p className="pb-lg-2" letter-spacing="#393f81;">Alredy have an account? <a href="#!"
+                                            color="#393f81;">Signin</a></p>
+
+                                        <div class="form-outline mb-4">
+                                            <label className="form-label" for="">User Name </label>
+                                            <input type="text" id="form2Example17" className=" register-inputfieldwidth form-control form-control-lg" />
+                                            
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <label class="form-label" for="form2Example27">E-mail</label>
+                                            <input type="email" id="form2Example27" class=" register-inputfieldwidth form-control form-control-lg" />
+                                           
+                                        </div>
+                                        <div class="form-outline mb-4">
+                                            <label class="form-label" for="form2Example27">Password</label>
+                                            <input type="password" id="form2Example27" class=" register-inputfieldwidth form-control form-control-lg" />
+                                           
+                                        </div>
+                                        <div class="form-outline mb-4">
+                                            <label class="form-label" for="form2Example27">Confirm Password</label>
+                                            <input type="password" id="form2Example27" class=" register-inputfieldwidth form-control form-control-lg" />
                                            
                                         </div>
 
-                                        <h5 className="fw-normal mb-3 pb-3" letter-spacing="1px;">Sign into your account</h5>
+                                        <div class="pt-1 mb-4 mt-5 text-center">
+                                            <button className ="register.loginbuttonsize btn btn-success "  type="button">REGISTER</button>
+                                        </div>
+                                        
 
-                                        <div class="form-outline mb-4">
-                                            <input type="email" id="form2Example17" className="form-control form-control-lg" />
-                                            <label className="form-label" for="">User Name</label>
-                                        </div>
-                                        <div class="form-outline mb-4">
-                                            <input type="email" id="form2Example17" className="form-control form-control-lg" />
-                                            <label className="form-label" for="">Email</label>
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <input type="password" id="form2Example27" class="form-control form-control-lg" />
-                                            <label class="form-label" for="form2Example27">Password</label>
-                                        </div>
-                                        <div class="form-outline mb-4">
-                                            <input type="password" id="form2Example27" class="form-control form-control-lg" />
-                                            <label class="form-label" for="form2Example27">Confirm Password</label>
-                                        </div>
-
-                                        <div class="pt-1 mb-4">
-                                            <button className ="btn btn-success btn-lg btn-block" type="button">Register</button>
-                                        </div>
-
-                                        {/* <a className="small text-muted" href="#!">Forgot password?</a>
-                                        <p className="mb-5 pb-lg-2" letter-spacing="#393f81;">Don't have an account? <a href="#!"
-                                            color="#393f81;">Register here</a></p>
-                                        <a href="#!" className="small text-muted">Terms of use.</a>
-                                        <a href="#!" className="small text-muted">Privacy policy</a> */}
+                                       
+                                        
+                                      
                                         </form>
 
                                     </div>
