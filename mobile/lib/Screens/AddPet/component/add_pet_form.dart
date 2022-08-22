c1759b94c0580a254e7ef613da894220c25fd4cd
ThilinaVP <thilinavp@gmail.com>
2022-08-23 03:00:09 +0530

    add pet form date picker added

--- mobile/lib/Screens/AddPet/component/add_pet_form.dart
+++ mobile/lib/Screens/AddPet/component/add_pet_form.dart
@@ -12,6 +12,13 @@ class AddPetForm extends StatefulWidget {
   State<StatefulWidget> createState() => _AddPetFormState();
 }
 class _AddPetFormState extends State<AddPetForm>{
+  late DateTime? _dateTime = null;
+  String initialCat = 'Dog';
+  var Catagories = [
+    'Dog',
+    'Cat',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -19,6 +26,21 @@ class _AddPetFormState extends State<AddPetForm>{
         width: MediaQuery.of(context).size.width -80,
         child: Column(
         children: [
+          Padding(
+              padding: const EdgeInsets.only(top: 20),
+            child: Container(
+              child: Row(
+                children: const [
+                  Text(
+                      "Pet Name",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              )
+            ),
+          ),
           TextFormField(
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
@@ -26,67 +48,131 @@ class _AddPetFormState extends State<AddPetForm>{
             onSaved: (email) {},
             decoration: const InputDecoration(
               border: UnderlineInputBorder(),
-              labelText: 'Enter your username',
+              labelText: 'Enter your pet\'s name here',
               labelStyle: TextStyle(
                 color: Color(0xFFD6D6D6),
               ),
               fillColor: Colors.white,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child: TextFormField(
-              textInputAction: TextInputAction.done,
-              obscureText: true,
-              cursorColor: formBG,
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: 'Enter your username',
-                labelStyle: TextStyle(
-                  color: Color(0xFFD6D6D6),
+
+          Row(
+            children: [
+
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: defaultPadding,horizontal: 10),
+                child: Text(
+                    "DOB",
+                  style: TextStyle(
+                    fontSize: 18,
+
+                  ),
+
                 ),
-                fillColor: Colors.white,
               ),
+              Spacer(),
+              Padding(
+              padding: const EdgeInsets.symmetric(vertical: defaultPadding,horizontal: 10),
+              child: Text(_dateTime == null? "Select Date" : _dateTime.toString()),
+            ),
+              Spacer(),
+              ElevatedButton(
+                  onPressed: (){
+                    showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate:  DateTime.now()
+                    ).then((date){
+                        setState(() {
+                          _dateTime = date;
+                        });
+                    });
+                  },
+                  child: Text("Select")
+              ),
+
+            ],
+          ),
+          Container(
+            child: Column(
+              children: [
+                Row(
+                  children: const [
+                    Text(
+                      "Select pet Catagory",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width -80,
+                  child: DropdownButton(
+
+                    // Initial Value
+                    value: initialCat,
+
+                    // Down Arrow Icon
+
+                    icon: const Icon(
+                        Icons.keyboard_arrow_down,
+                    ),
+
+                    // Array list of items
+                    items: Catagories.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    // After selecting the desired option,it will
+                    // change button value to selected value
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        initialCat = newValue!;
+                      }
+                      );
+                    },
+                  ),
+                )
+              ],
             ),
           ),
 
-          Hero(
-              tag: "login_btn",
-              child:SizedBox(
-                width: 100,
-                height: 30,
-                child:ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return DashboardScreen();
-                        },
+
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Container(
+                child: Row(
+                  children: const [
+                    Text(
+                      "Weight",
+                      style: TextStyle(
+                        fontSize: 18,
                       ),
-                    );
-                  },
-                  child: Text(
-                    "Login".toUpperCase(),
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              )
+                    ),
+                  ],
+                )
+            ),
           ),
-          const SizedBox(height: defaultPadding),
-          AlreadyHaveAnAccountCheck(
-            press: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return SignUpScreen();
-                  },
-                ),
-              );
-            },
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: formBG,
+            onSaved: (email) {},
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your pet\'s Weight here',
+              labelStyle: TextStyle(
+                color: Color(0xFFD6D6D6),
+              ),
+              fillColor: Colors.white,
+            ),
           ),
         ],
+
       ),
       ),
     );
