87c139765a98624740a9f2f29b9488b6446c345f
vihanga-sen <sandunisenarath98@gmail.com>
2022-08-25 01:18:22 +0530

    added data to maderator dashboard

--- client/src/pages/mdpostsection.js
+++ client/src/pages/mdpostsection.js
@@ -41,7 +41,7 @@ function moderatordashboard() {
                                                     Pending<br/>Posts
                                                 </div><div class="vr"></div>
                                                 <div class='count'>
-                                                03
+                                                02
                                                 </div>
                                             </div></a>
 											{/* <div class="card-footer d-flex align-items-center justify-content-between">
