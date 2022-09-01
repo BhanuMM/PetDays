1558446dab68a2ebad3ddd4fcce076d55c17bc8d
hirushika <hirushikawickramaarachchi@gmail.com>
2022-09-02 00:40:48 +0530

    updated tables

--- client/src/App.js
+++ client/src/App.js
@@ -108,6 +108,7 @@ import Mdverifiedsingle from './pages/mdverifiedsingle';
 // import Newdashboard from './pages/newdashboard';
 
 import Spdashboard from './pages/spdashboard';
+import Test from './pages/test';
 
 
 
@@ -220,6 +221,7 @@ function App() {
           <Route path="/editservice" element={<Editservice/>}/>
           <Route path="/viewad" element={<Singlead/>}/>
           <Route path="/mdverifiedsingle" element={<Mdverifiedsingle/>}/>
+          <Route path="/test" element={<Test/>}/>
           {/* <Route path="/newdashboard" element={<Newdashboard/>}/> */}
 
         </Routes>   
