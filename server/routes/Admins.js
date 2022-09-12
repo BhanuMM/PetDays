a29bef660ac3d5a78f5a4f5fdb7864766928ff13
Bhanuji Malith Meegoda <49988198+BhanuMM@users.noreply.github.com>
2022-09-13 02:15:14 +0530

    register func

--- server/routes/Admins.js
+++ server/routes/Admins.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
-const { Petcatagories , Breeds } = require("../models");
+const { Petcatagories , Breeds ,Moderators } = require("../models");
+const registerUser = require("./Users.js")
 // const breeds = require('../models/breeds');
 
 // router.use(bodyParser.json());
@@ -44,15 +45,14 @@ router.post("/addbreed", async (req, res) => {
   
 });
 router.post("/addmoderator", async (req, res) => {
-  const { breedName,descr, catId } = req.body;
-  // const uemail = await Users.findOne({ where: { email: email } });
-  // const uname = await Users.findOne({ where: { username: username } });
-
-  // if (uemail) res.json({ error: "Email is already registered" });
- const chckq = Breeds.create({
-    breedName: breedName,
-    descr: descr,
-    catId: catId
+  const { firstName,lastName,conNum,modEmail,modNIC } = req.body;
+  
+ const chckq = Moderators.create({
+    firstName: firstName,
+    lastName: lastName,
+    conNum: conNum,
+    modEmail:modEmail,
+    modNIC:modNIC
 
   });
   if(chckq){
