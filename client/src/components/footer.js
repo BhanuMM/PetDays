f7b998a52382e89ed91ec0c3bd36cae656177548
Bhanuji Malith Meegoda <49988198+BhanuMM@users.noreply.github.com>
2022-10-23 05:40:35 +0530

    index changes

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -6,11 +6,11 @@ function footer() {
     <div className="main-footer">
      <div className="container-fluid">
         <div className="row mx-auto">
-          <div className="col-md-6 mt-5 pl-5">
-          <h5 className="text-uppercase footer-topics text-center fs-6 paratitle">our services</h5>
+          <div className="col-md-5 mt-5 pl-5">
+          <h5 className="text-uppercase footer-topics text-center fs-6 paratitle pb-3">our services</h5>
           <ul className="list-unstyled text-center ">
               <li>
-                <a href="registertype" className="text-black ">Register</a>
+                <a href="home" className="text-black ">Home</a>
               </li>
               <li>
                 <a href="forum" className="text-black">Pet Talk</a>
@@ -18,13 +18,14 @@ function footer() {
               <li>
                 <a href="petmart" className="text-black">Pet Mart</a>
               </li>
+              
             </ul>
           </div>
-          <div className="col-md-6 mt-5 pl-5">
-          <h5 className="text-uppercase footer-topics fs-6 text-left paratitle">About Us</h5>
+          <div className="col-md-7 mt-5 pl-5">
+          <h5 className="text-uppercase footer-topics fs-6 text-left paratitle pb-3">About Us</h5>
           <ul className="list-unstyled text-left">
           <p className='para'>Pet Days is a web and mobile application that centralizes all the needs of a pet owner living in Sri Lanka. Pet Days provide the necessary health care details and help you keep track of all the
-  medication, vaccines, and dietary needs of your pet.</p>
+  medication, vaccines, and dietary needs of your pet. <br/> Simply ALL IN ONE !</p>
             </ul>
           </div>
         </div>
@@ -33,7 +34,7 @@ function footer() {
         <img src= {cat} className="mx-auto footer-image" alt="dog"/>
       </div>
       <div className="text-center p-3">
-       <h1 className="footer-text"> © 2022 Copyright:</h1>
+       <h1 className="footer-text"> © 2022 Copyright: Petdays</h1>
       </div>
     </div>
     
